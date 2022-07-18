9ec6e9768e130401c6675637998ab7e256b5a5da
Zacory Shakespear <zshakespear@gmail.com>
2022-07-18 12:05:23 -0600

    Debugged MatchTest and wrote check method

--- src/parser.py
+++ src/parser.py
@@ -24,18 +24,24 @@ class Parser():
         self.rules_list()
         self.match(t.tokenType.COMMANDS)
         self.match(t.tokenType.COLON)
+        self.check()
         self.commands_list()
-        
+    
+    def check(self):
+        if len(self.tokens) == 0:
+            raise Exception('ran out of tokens')
         
     def match(self, target_token):
+        self.check()
         if self.tokens[0].type == target_token:
             output = self.tokens[0]
             self.tokens.pop(0)
             return output
         else:
-            raise Exception('token not found')
+            raise Exception(str(target_token)+'not found')
         
     def rules_list(self):
+        self.check()
         if(self.tokens[0] == t.tokenType.NONTERMINAL):
             self.rule()
             self.rules_list()
@@ -50,15 +56,19 @@ class Parser():
             return
         
     def rule(self):
+        self.check()
         print('you haven\'t defined rule yet')
         
     def command(self):
+        self.check()
         print('you havent defined command yet')
         
     def product(self):
+        self.check()
         print('you havent defined product yet')
         
     def product_list(self):
+        self.check()
         if (self.tokens[0] == t.tokenType.TERMINAl 
             or self.tokens[0] == t.tokenType.NONTERMINAL
             or self.tokens[0] == t.tokenType.OR):
@@ -68,12 +78,38 @@ class Parser():
             return
         
 def MatchTest():
-    token_list = [t.token(t.tokenType.RULES,'RULES',0),
-                  t.token(t.tokenType.COLON,':',0),
-                  t.token(t.tokenType.COMMANDS,'COMMANDS',1),
-                  t.token(t.tokenType.COLON,':',1)]
-    p = Parser(token_list)
-    p.run()
-    for el in p.tokens:
-        print(el.type)
+    try:
+        token_list = [t.token(t.tokenType.RULES,'RULES',0),
+                      t.token(t.tokenType.COLON,':',0),
+                      t.token(t.tokenType.COMMANDS,'COMMANDS',1),
+                      t.token(t.tokenType.COLON,':',1),
+                      t.token(t.tokenType.NONTERMINAL,'SYL',2)]
+        p = Parser(token_list)
+        p.run()
+        
+    except Exception:
+        print('Test 1 Failed')
+    else:
+        for el in p.tokens:
+            print(el.type)
+    try:
+        token_list = [t.token(t.tokenType.RULES,'RULES',0),
+                      t.token(t.tokenType.COLON,':',0)]
+        p = Parser(token_list)
+        p.run()
+    except Exception:
+        print('Test 2 Failed')
+    else:
+        print('Something\'s wrong')
     
+    try:
+        token_list = [t.token(t.tokenType.COMMANDS,'COMMANDS',1),
+                      t.token(t.tokenType.COLON,':',1)]
+        p = Parser(token_list)
+        p.run()
+    except Exception:
+        print('Test 3 Failed')
+    else:
+        print('Something\'s wrong')
+        
+MatchTest()
